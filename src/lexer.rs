63a3b13758e2a88d727d08a1407cd82f8e41a999
999SH <viktor.salomonsson12@gmail.com>
2023-06-07 21:15:40 +0200

    Lexer and tests updated

--- src/lexer.rs
+++ src/lexer.rs
@@ -11,10 +11,10 @@ pub enum TokenType {
     For,
     Return,
     Void,
-    //Switch,
-    //Case,
-    //Struct,
-    //Union,
+    Switch,
+    Case,
+    Struct,
+    Union,
     Newline,
     Tab,
     EscapeChar(char),
@@ -28,22 +28,22 @@ pub enum TokenType {
     StringLiteral(String),
 
     // Operators
-    Plus,         // "+"
-    Minus,        // "-"
-    Star,         // "*"
-    Slash,        // "/"
-    Percent,      // "%"
-    Equal,        // "="
-    Less,         // "<"
-    Greater,      // ">"
-    EqualEqual,   // "=="
-    NotEqual,     // "!="
-    LessEqual,    // "<="
-    GreaterEqual, // ">="
+    Plus,         // "+" X
+    Minus,        // "-" X
+    Star,         // "*" X
+    Slash,        // "/" X
+    Percent,      // "%" X
+    Equal,        // "=" X
+    Less,         // "<" X
+    Greater,      // ">" X
+    EqualEqual,   // "==" X
+    NotEqual,     // "!=" X
+    LessEqual,    // "<=" X
+    GreaterEqual, // ">=" X
     AndAnd,       // "&&"
     OrOr,         // "||"
     Not,          // "!"
-    And,          // "&"
+    Ampersand,    // "&"
     Or,           // "|"
     Xor,          // "^"
     Tilde,        // "~"
@@ -103,16 +103,55 @@ impl<'a> Lexer<'a> {
             '-' => TokenType::Minus,
             '*' => TokenType::Star,
             '/' => TokenType::Slash,
-            '=' => TokenType::Equal,
+            '=' => match self.peek_char() {
+                '=' => {
+                    self.read_char();
+                    TokenType::EqualEqual
+                }
+                _ => TokenType::Equal,
+            },
+            '!' => match self.peek_char() {
+                '=' => {
+                    self.read_char();
+                    TokenType::NotEqual
+                }
+                _ => TokenType::Not,
+            },
+            '%' => TokenType::Percent,
+            '<' => TokenType::Less,
+            '>' => match self.peek_char() {
+                '=' => {
+                    self.read_char();
+                    TokenType::GreaterEqual
+                }
+                '>' => {
+                    self.read_char();
+                    TokenType::ShiftRight
+                }
+                _ => TokenType::Greater,
+            },
+            //'<=' => TokenType::LessEqual,
+            //'<<' => TokenType::ShiftLeft,
+            '&' => TokenType::Ampersand,
+            '|' => TokenType::Or,
 
             //Keywords
             'a'..='z' | 'A'..='Z' | '_' => {
                 let identifier: String = self.get_keyword();
                 match identifier.as_str() {
+                    "void" => TokenType::Void,
                     "int" => TokenType::Int,
+                    "char" => TokenType::Char,
                     "if" => TokenType::If,
                     "return" => TokenType::Return,
                     "else" => TokenType::Else,
+                    "while" => TokenType::While,
+                    "for" => TokenType::For,
+                    "Switch" => TokenType::Switch,
+                    "Case" => TokenType::Case,
+                    "Struct" => TokenType::Struct,
+                    "Union" => TokenType::Union,
+
                     _ => TokenType::Identifier(identifier),
                 }
             }
@@ -122,7 +161,6 @@ impl<'a> Lexer<'a> {
                 TokenType::IntConst(int)
             }
             //Newline
-
 
             //End of file
             '\0' => TokenType::EOF,
@@ -191,9 +229,18 @@ mod tests {
         let input = "foo bar baz";
         let mut lexer = Lexer::new(input);
 
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("foo".to_string())));
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("bar".to_string())));
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("baz".to_string())));
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("foo".to_string()))
+        );
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("bar".to_string()))
+        );
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("baz".to_string()))
+        );
     }
 
     #[test]
@@ -223,9 +270,48 @@ mod tests {
         let input = "  foo   bar\tbaz\nqux";
         let mut lexer = Lexer::new(input);
 
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("foo".to_string())));
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("bar".to_string())));
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("baz".to_string())));
-        assert_eq!(lexer.next_token(), Some(TokenType::Identifier("qux".to_string())));
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("foo".to_string()))
+        );
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("bar".to_string()))
+        );
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("baz".to_string()))
+        );
+        assert_eq!(
+            lexer.next_token(),
+            Some(TokenType::Identifier("qux".to_string()))
+        );
+    }
+
+    #[test]
+    fn lexer_asserts_equals() {
+        let input = "== =";
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next_token(), Some(TokenType::EqualEqual));
+        assert_eq!(lexer.next_token(), Some(TokenType::Equal));
+    }
+    #[test]
+    fn lexer_asserts_not_equals() {
+        let input = "!= !";
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next_token(), Some(TokenType::NotEqual));
+        assert_eq!(lexer.next_token(), Some(TokenType::Not));
+    }
+
+    #[test]
+    fn lexer_asserts_greater_and_co() {
+        let input = "> >> >=";
+        let mut lexer = Lexer::new(input);
+
+        assert_eq!(lexer.next_token(), Some(TokenType::Greater));
+        assert_eq!(lexer.next_token(), Some(TokenType::ShiftRight));
+        assert_eq!(lexer.next_token(), Some(TokenType::GreaterEqual));
     }
 }
